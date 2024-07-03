eabc2733fd49083a4d4fd5faacf052ed9efda1cc
Zaahiid <zahidrsheikh01@gmail.com>
2024-07-04 00:26:01 +0530

    Added Token Validation 🍪

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,33 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
   return (
-    <div className='bg-blue-800 py-6'>
-        <div className='container mx-auto flex justify-between'>
-            <span className="text-3xl text-white font-bold tracking-tight">
-                <Link to="/">Zholidays.com</Link>
-            </span>
-            <span className="flex space-x-2">
-                <Link to="/sign-in" className='flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100 hover:text-green-500'>
-                    Sign In
-                </Link>
-            </span>
-        </div>
+    <div className="bg-blue-800 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">Zholidays.com</Link>
+        </span>
+        <span className="flex space-x-2">
+          {isLoggedIn ? (
+            <>
+              <Link to="/my-bookings">My Bookings</Link>
+              <Link to="/my-hotels">My Hotels</Link>
+              <button>Sign OUt</button>
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100 hover:text-green-500"
+            >
+              Sign In
+            </Link>
+          )}
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
